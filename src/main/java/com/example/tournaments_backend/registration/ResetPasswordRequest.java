3749a75aba2bc866dd04221f16efa76430608ee9
Heri <hrodriguez1821@gmail.com>
2025-04-08 17:06:22 -0700

    Created a class to represent the req body for POST /reset-password. #1

---
+++ src/main/java/com/example/tournaments_backend/registration/ResetPasswordRequest.java
@@ -0,0 +1,25 @@
+package com.example.tournaments_backend.registration;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor 
+@ToString
+public class ResetPasswordRequest {
+    @NotNull
+    @NotEmpty
+    private final String token;
+    @ValidEmail
+    @NotNull
+    @NotEmpty
+    private final String email;
+    @NotNull
+    @NotEmpty
+    private final String newPassword;
+}
+
