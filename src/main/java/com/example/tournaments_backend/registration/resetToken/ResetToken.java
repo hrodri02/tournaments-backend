9d011e14e2b0a2610a8be69fcf019e53fb86d6c4
Heri <hrodriguez1821@gmail.com>
2025-04-08 17:00:17 -0700

    Added a column to save when someone's password has been reset. #1

--- src/main/java/com/example/tournaments_backend/registration/resetToken/ResetToken.java
+++ src/main/java/com/example/tournaments_backend/registration/resetToken/ResetToken.java
@@ -38,6 +38,8 @@ public class ResetToken {
     private LocalDateTime createdAt;
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
+    @Column(name = "reset_at")
+    private LocalDateTime resetAt;
     @ManyToOne
     @JoinColumn(
         name = "app_user_id", 
