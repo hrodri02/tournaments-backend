f945245122bfdd008653ddf110d763a17c9ae49b
Heri <hrodriguez1821@gmail.com>
2025-04-08 17:01:55 -0700

    Added methods to get a ResetToken by token and user, to set a ResetTokens resetAt property, and to invalidate unused tokens.

--- src/main/java/com/example/tournaments_backend/registration/resetToken/ResetTokenRepository.java
+++ src/main/java/com/example/tournaments_backend/registration/resetToken/ResetTokenRepository.java
@@ -1,8 +1,16 @@
 package com.example.tournaments_backend.registration.resetToken;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.tournaments_backend.app_user.AppUser;
+
 @Repository
 public interface ResetTokenRepository extends JpaRepository<ResetToken, Long> {
+    Optional<ResetToken> findByTokenAndAppUser(String token, AppUser user);
+    Optional<ResetToken> findByToken(String token);
+    List<ResetToken> findByAppUserAndResetAtIsNull(AppUser appUser);
 }
--- src/main/java/com/example/tournaments_backend/registration/resetToken/ResetTokenService.java
+++ src/main/java/com/example/tournaments_backend/registration/resetToken/ResetTokenService.java
@@ -1,5 +1,12 @@
 package com.example.tournaments_backend.registration.resetToken;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.example.tournaments_backend.app_user.AppUser;
+import com.example.tournaments_backend.registration.token.ConfirmationToken;
 
 import lombok.AllArgsConstructor;
 
@@ -10,5 +17,27 @@ public class ResetTokenService {
 
     public void saveResetToken(ResetToken token) {
         resetTokenRepository.save(token);
+    }
+
+    public Optional<ResetToken> getToken(String token, AppUser user) {
+        return resetTokenRepository.findByTokenAndAppUser(token, user);
+    }
+
+    public void setResetAt(String token) {
+        ResetToken resetToken = 
+            resetTokenRepository
+                .findByToken(token)
+                .orElseThrow(() -> new IllegalStateException("token not found"));
+            
+        resetToken.setResetAt(LocalDateTime.now());
+        resetTokenRepository.save(resetToken);
+    }
+
+    public void invalidateUnconfirmedTokens(AppUser appUser) {
+        List<ResetToken> tokens = resetTokenRepository.findByAppUserAndResetAtIsNull(appUser);
+        for (ResetToken token : tokens) {
+            token.setExpiresAt(LocalDateTime.now()); // Mark old tokens as expired
+            resetTokenRepository.save(token);
+        }
     }
 }
