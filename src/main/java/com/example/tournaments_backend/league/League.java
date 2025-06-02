08fe375b03b2961fcaab4ee211a38cbb70615328
Heri <hrodriguez1821@gmail.com>
2025-06-02 10:56:24 -0700

    Added classes for league controller, service, repo, and league.

---
+++ src/main/java/com/example/tournaments_backend/league/League.java
@@ -0,0 +1,35 @@
+package com.example.tournaments_backend.league;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class League {
+    @Id
+    @SequenceGenerator(
+        name="league_sequence",
+        sequenceName="league_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "league_sequence"
+    )
+    private Long id;
+    private String name;
+
+    public League(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/tournaments_backend/league/LeagueService.java
@@ -0,0 +1,8 @@
+package com.example.tournaments_backend.league;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeagueService {
+    
+}
