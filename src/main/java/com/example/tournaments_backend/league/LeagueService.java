556603063d079ace6b9fb698e278e091da4205ae
Heri <hrodriguez1821@gmail.com>
2025-06-03 08:23:37 -0700

    Removed the check for an existing league by id since the league passed to the post request will not have an id property. Returning the the league saved in the db which includes the generated id.

--- src/main/java/com/example/tournaments_backend/league/LeagueService.java
+++ src/main/java/com/example/tournaments_backend/league/LeagueService.java
@@ -1,8 +1,15 @@
 package com.example.tournaments_backend.league;
 
 import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class LeagueService {
-    
+    private final LeagueRepository leagueRepository;
+
+    public League addLeague(League league) {
+        League leagueInDB = leagueRepository.save(league);
+        return leagueInDB;
+    }    
 }
