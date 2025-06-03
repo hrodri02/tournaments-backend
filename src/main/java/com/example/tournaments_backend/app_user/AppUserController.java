ae8592107e9ebc06c558326d2c03a87da2019268
Heri <hrodriguez1821@gmail.com>
2025-06-03 09:02:47 -0700

    Removed unused import.

--- src/main/java/com/example/tournaments_backend/app_user/AppUserController.java
+++ src/main/java/com/example/tournaments_backend/app_user/AppUserController.java
@@ -3,7 +3,6 @@ package com.example.tournaments_backend.app_user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.Authentication;
