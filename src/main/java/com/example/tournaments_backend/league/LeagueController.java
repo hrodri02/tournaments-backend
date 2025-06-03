19140af32e61ea6307a14a3d328216002995c30c
Heri <hrodriguez1821@gmail.com>
2025-06-03 08:25:15 -0700

    Implemented post /league to be able to create a new league.

---
+++ src/main/java/com/example/tournaments_backend/league/LeagueController.java
@@ -0,0 +1,25 @@
+package com.example.tournaments_backend.league;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/v1/leagues")
+public class LeagueController {
+    private final LeagueService leagueService;
+
+    @Autowired
+    public LeagueController(LeagueService leagueService) {
+        this.leagueService = leagueService;
+    }
+
+    @PostMapping
+    public ResponseEntity<League> addLeague(@RequestBody League league) {
+        League leagueInDB =leagueService.addLeague(league);
+        return ResponseEntity.ok().body(leagueInDB);
+    }
+}
