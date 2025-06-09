d20f9dd0d15df44b433ccf746b707bed968f4e4c
Heri <hrodriguez1821@gmail.com>
2025-06-09 13:46:03 -0700

    Removed unused imports.

--- src/main/java/com/example/tournaments_backend/team/TeamController.java
+++ src/main/java/com/example/tournaments_backend/team/TeamController.java
@@ -1,13 +1,8 @@
 package com.example.tournaments_backend.team;
 
-import java.util.Date;
-
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.tournaments_backend.exception.ErrorDetails;
 import com.example.tournaments_backend.exception.TeamNotFoundException;
 
 import jakarta.validation.Valid;
