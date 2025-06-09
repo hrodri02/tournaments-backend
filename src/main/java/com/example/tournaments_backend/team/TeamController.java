917218c586b62e397e521f4729648884b3cd53fc
Heri <hrodriguez1821@gmail.com>
2025-06-09 11:32:02 -0700

    Implemented endpoint to get a team by id.

--- src/main/java/com/example/tournaments_backend/team/TeamController.java
+++ src/main/java/com/example/tournaments_backend/team/TeamController.java
@@ -1,11 +1,20 @@
 package com.example.tournaments_backend.team;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tournaments_backend.exception.ErrorDetails;
+import com.example.tournaments_backend.exception.TeamNotFoundException;
 
 import jakarta.validation.Valid;
 
@@ -21,7 +30,13 @@ public class TeamController {
 
     @PostMapping
     public ResponseEntity<Team> addTeam(@RequestBody @Valid Team team) {
-        Team teamInDB =teamService.addTeam(team);
+        Team teamInDB = teamService.addTeam(team);
         return ResponseEntity.ok().body(teamInDB);
+    }
+
+    @GetMapping("{teamId}")
+    public ResponseEntity<Team> getTeam(@PathVariable("teamId") Long teamId) throws TeamNotFoundException {
+        Team team = teamService.getTeamById(teamId);
+        return ResponseEntity.ok().body(team);
     }
 }
