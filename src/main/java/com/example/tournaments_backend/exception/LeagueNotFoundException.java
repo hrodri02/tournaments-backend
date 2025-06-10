6a2225b95116007481c66b43c023b6a5b703fe27
Heri <hrodriguez1821@gmail.com>
2025-06-10 08:46:14 -0700

    Removed unused exception class.

--- src/main/java/com/example/tournaments_backend/exception/LeagueNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.tournaments_backend.exception;
-
-public class LeagueNotFoundException extends Exception {
-    public LeagueNotFoundException(String message) {
-        super(message);
-    }
-}
