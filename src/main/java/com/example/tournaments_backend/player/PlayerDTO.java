e3c5068922b952a00e2c61ad15465123ae2b0121
Heri <hrodriguez1821@gmail.com>
2025-06-17 16:56:49 -0700

    Created a type to return certain properties of a Player.

---
+++ src/main/java/com/example/tournaments_backend/player/PlayerDTO.java
@@ -0,0 +1,15 @@
+package com.example.tournaments_backend.player;
+
+import com.example.tournaments_backend.app_user.AppUserRole;
+import com.example.tournaments_backend.app_user.UserDTO;
+
+import lombok.Getter;
+
+@Getter
+public class PlayerDTO extends UserDTO {
+    private Position position;
+    public PlayerDTO(Long id, String firstName, String lastName, String email, AppUserRole appUserRole, Position position) {
+        super(id, firstName, lastName, email, appUserRole);
+        this.position = position;
+    }
+}
