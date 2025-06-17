3fab39ad76ee4f7e3c803a16d1e7bb2c5959cfa9
Heri <hrodriguez1821@gmail.com>
2025-06-17 16:22:17 -0700

    Added method to save a player in the db.

--- src/main/java/com/example/tournaments_backend/player/PlayerService.java
+++ src/main/java/com/example/tournaments_backend/player/PlayerService.java
@@ -1,11 +1,15 @@
 package com.example.tournaments_backend.player;
 
 import org.springframework.stereotype.Service;
-
 import lombok.AllArgsConstructor;
+import com.example.tournaments_backend.exception.ServiceException;
 
 @Service
 @AllArgsConstructor
 public class PlayerService {
     private final PlayerRepository playerRepository;
+
+    public Player save(Player player) throws ServiceException {
+        return playerRepository.save(player);
+    }
 }
