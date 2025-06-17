f5290b0429606b31e4db3fb669d878cf35c2cdcf
Heri <hrodriguez1821@gmail.com>
2025-06-17 16:57:44 -0700

    Added functionality to get a player by id.

--- src/main/java/com/example/tournaments_backend/player/PlayerController.java
+++ src/main/java/com/example/tournaments_backend/player/PlayerController.java
@@ -1,8 +1,13 @@
 package com.example.tournaments_backend.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tournaments_backend.exception.ServiceException;
 
 @RestController
 @RequestMapping(path="/api/v1/players")
@@ -12,5 +17,18 @@ public class PlayerController {
     @Autowired
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
+    }
+
+    @GetMapping("{playerId}")
+    public ResponseEntity<PlayerDTO> getPlayer(@PathVariable("playerId") Long playerId) throws ServiceException {
+        Player player = playerService.getPlayerById(playerId);
+        PlayerDTO playerDTO = 
+            new PlayerDTO(playerId, 
+                          player.getFirstName(),
+                          player.getLastName(),
+                          player.getEmail(), 
+                          player.getAppUserRole(),
+                          player.getPosition());
+        return ResponseEntity.ok().body(playerDTO);
     }
 }
--- src/main/java/com/example/tournaments_backend/player/PlayerService.java
+++ src/main/java/com/example/tournaments_backend/player/PlayerService.java
@@ -2,6 +2,8 @@ package com.example.tournaments_backend.player;
 
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
+
+import com.example.tournaments_backend.exception.ErrorType;
 import com.example.tournaments_backend.exception.ServiceException;
 
 @Service
@@ -9,7 +11,15 @@ import com.example.tournaments_backend.exception.ServiceException;
 public class PlayerService {
     private final PlayerRepository playerRepository;
 
-    public Player save(Player player) throws ServiceException {
+    public Player save(Player player) {
         return playerRepository.save(player);
+    }
+
+    public Player getPlayerById(Long id) throws ServiceException {
+        Player player = 
+            playerRepository
+                .findById(id)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "Player", "Player with given id not found."));
+        return player;
     }
 }
