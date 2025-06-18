f886e7c306b3bac65bdd109a959a7833e8eb4b52
Heri <hrodriguez1821@gmail.com>
2025-06-18 10:16:57 -0700

    A type that returns a team with a list of playerDTO.

---
+++ src/main/java/com/example/tournaments_backend/team/TeamDTO.java
@@ -0,0 +1,25 @@
+package com.example.tournaments_backend.team;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.tournaments_backend.player.PlayerDTO;
+
+import lombok.Getter;
+
+@Getter
+public class TeamDTO {
+    private Long id;
+    private String name;
+    private List<PlayerDTO> players; // List of PlayerDTOs
+
+    // Constructor to map from Team entity
+    public TeamDTO(Team team) {
+        this.id = team.getId();
+        this.name = team.getName();
+        // Use stream to map Player entities to PlayerDTOs
+        this.players = team.getPlayers().stream()
+                             .map(PlayerDTO::new) // Call the PlayerDTO constructor that takes a Player entity
+                             .collect(Collectors.toList());
+    }
+}
