ca92f8e49ccb83772640719a45fd0d04164c43b1
Heri <hrodriguez1821@gmail.com>
2025-06-25 08:56:34 -0700

    Implemented endpoint to create game.

--- src/main/java/com/example/tournaments_backend/game/GameController.java
+++ src/main/java/com/example/tournaments_backend/game/GameController.java
@@ -1,10 +1,22 @@
 package com.example.tournaments_backend.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tournaments_backend.exception.ErrorDetails;
+import com.example.tournaments_backend.exception.ServiceException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path="/api/v1/games")
@@ -15,5 +27,21 @@ public class GameController {
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    @Operation(summary = "Create a game", description = "Returns the game created")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully created a game", 
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = GameDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid - game is not valid",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized - not authenticated",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class)))
+    })
+    @PostMapping
+    public ResponseEntity<GameDTO> addGame(@RequestBody @Valid CreateGameRequest requestBody) throws ServiceException {
+        Game gameInDB = gameService.addGame(requestBody);
+        GameDTO gameDTO = new GameDTO(gameInDB);
+        return ResponseEntity.ok().body(gameDTO);
     }
 }
--- src/main/java/com/example/tournaments_backend/game/GameService.java
+++ src/main/java/com/example/tournaments_backend/game/GameService.java
@@ -2,10 +2,42 @@ package com.example.tournaments_backend.game;
 
 import org.springframework.stereotype.Service;
 
+import com.example.tournaments_backend.exception.ErrorType;
+import com.example.tournaments_backend.exception.ServiceException;
+import com.example.tournaments_backend.league.League;
+import com.example.tournaments_backend.league.LeagueRepository;
+import com.example.tournaments_backend.team.Team;
+import com.example.tournaments_backend.team.TeamRepository;
+
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class GameService {
     private final GameRepository gameRepository;
+    private final TeamRepository teamRepository;
+    private final LeagueRepository leagueRepository;
+
+    public Game addGame(CreateGameRequest gameRequest) throws ServiceException {
+        Game game = new Game(gameRequest);
+        Long homeTeamId = gameRequest.getHomeTeamId();
+        Long awayTeamId = gameRequest.getAwayTeamId();
+        Long leagueId = gameRequest.getLeagueId();
+        Team homeTeam = 
+            teamRepository
+                .findById(homeTeamId)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "Team", "Team with given id not found"));
+        Team awayTeam = 
+            teamRepository
+                .findById(awayTeamId)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "Team", "Team with given id not found"));
+        League league = 
+            leagueRepository
+                .findById(leagueId)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "League", "League with given id not found"));
+        game.setHomeTeam(homeTeam);
+        game.setAwayTeam(awayTeam);
+        game.setLeague(league);
+        return gameRepository.save(game);
+    }
 }
