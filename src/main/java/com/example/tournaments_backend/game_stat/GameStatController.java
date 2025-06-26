60497fa0dce0b61457214ce3cadc8a0a1b591eec
Heri <hrodriguez1821@gmail.com>
2025-06-26 09:13:37 -0700

    Implemented POST /gamestats. Add http method to test creating a game stat.

--- src/main/java/com/example/tournaments_backend/game_stat/GameStatController.java
+++ src/main/java/com/example/tournaments_backend/game_stat/GameStatController.java
@@ -1,10 +1,21 @@
 package com.example.tournaments_backend.game_stat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tournaments_backend.exception.ErrorDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path="/api/v1/gamestats")
@@ -15,5 +26,23 @@ public class GameStatController {
     @Autowired
     public GameStatController(GameStatService gameStatService) {
         this.gameStatService = gameStatService;
+    }
+
+    @Operation(summary = "Create a game stat", description = "Returns the game stat created")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully created a game stat", 
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = GameStatDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Invalid - game stat is not valid",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized - not authenticated",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class))),
+        @ApiResponse(responseCode = "404", description = "Not found - game or player with given ID not found",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDetails.class)))
+    })
+    @PostMapping
+    public ResponseEntity<GameStatDTO> addGameStat(@RequestBody @Valid GameStatRequest request) {
+        GameStat gameStat = gameStatService.addGameStat(request);
+        GameStatDTO gameStatDTO = new GameStatDTO(gameStat);
+        return ResponseEntity.ok().body(gameStatDTO);
     }
 }
--- src/main/java/com/example/tournaments_backend/game_stat/GameStatService.java
+++ src/main/java/com/example/tournaments_backend/game_stat/GameStatService.java
@@ -1,7 +1,12 @@
 package com.example.tournaments_backend.game_stat;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.tournaments_backend.exception.ServiceException;
+import com.example.tournaments_backend.exception.ErrorType;
+import com.example.tournaments_backend.game.Game;
+import com.example.tournaments_backend.player.Player;
 import com.example.tournaments_backend.game.GameRepository;
 import com.example.tournaments_backend.player.PlayerRepository;
 
@@ -13,4 +18,23 @@ public class GameStatService {
     private final GameStatRepository gameStatRepository;
     private final GameRepository gameRepository;
     private final PlayerRepository playerRepository;
+
+    @Transactional
+    public GameStat addGameStat(GameStatRequest gameStatRequest) throws ServiceException {
+        GameStat gameStat = new GameStat(gameStatRequest);
+        Long gameId = gameStatRequest.getGameId();
+        Game game = 
+            gameRepository
+                .findById(gameId)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "Game stat", "Game with id = " + gameId + " not found"));
+
+        Long playerId = gameStatRequest.getPlayerId();
+        Player player =
+            playerRepository
+                .findById(playerId)
+                .orElseThrow(() -> new ServiceException(ErrorType.NOT_FOUND, "Game stat", "Game with id = " + playerId + " not found"));
+        gameStat.setGame(game);
+        gameStat.setPlayer(player);   
+        return gameStatRepository.save(gameStat); 
+    }
 }
