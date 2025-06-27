7ae06310e1f2e28f1f75c2f391f8cbf3f9c357b4
Heri <hrodriguez1821@gmail.com>
2025-06-27 08:28:07 -0700

    A type that represents the reponse body when a League is returned.

---
+++ src/main/java/com/example/tournaments_backend/league/LeagueDTO.java
@@ -0,0 +1,32 @@
+package com.example.tournaments_backend.league;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.tournaments_backend.team.TeamDTO;
+
+import lombok.Getter;
+
+@Getter
+public class LeagueDTO {
+    private Long id;
+    private String name;
+    private LocalDate startDate;
+    private Integer durationInWeeks;
+    private List<TeamDTO> teams;
+
+    public LeagueDTO(League league) {
+        this.id = league.getId();
+        this.name = league.getName();
+        this.startDate = league.getStartDate();
+        this.durationInWeeks = league.getDurationInWeeks();
+        if (league.getTeams() != null) {
+            this.teams = 
+                league.getTeams()
+                    .stream()
+                    .map(TeamDTO::new)
+                    .collect(Collectors.toList());
+        }
+    }
+}
