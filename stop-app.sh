588f6595c05299ce678902d57f23644a659ac309
Heri <hrodriguez1821@gmail.com>
2025-07-15 09:34:48 -0700

    Replaced cut cmd with awk to get the process ID.

--- stop-app.sh
+++ stop-app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # stop the spring app
-ID=$(sudo lsof -i:8080 | sed -n '2p' | cut -d' ' -f2)
+ID=$(sudo lsof -i:8080 | sed -n '2p' | awk '{print $2}')
 sudo kill -9 $ID
 # stop the database
-ID=$(sudo lsof -i:5432 | sed -n '2p' | cut -d' ' -f2)
+ID=$(sudo lsof -i:5432 | sed -n '2p' | awk '{print $2}')
 sudo kill -9 $ID
 # stop the mail server
-ID=$(sudo lsof -i:1080 | sed -n '2p' | cut -d' ' -f2)
+ID=$(sudo lsof -i:1080 | sed -n '2p' | awk '{print $2}')
 sudo kill -9 $ID
