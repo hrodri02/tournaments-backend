55c81b839acab3ec455da7ad7e2a69e57cba8347
Heri <hrodriguez1821@gmail.com>
2025-07-15 08:30:27 -0700

    allow requests to one endpoint.

--- src/main/java/com/example/tournaments_backend/TournamentsBackendApplication.java
+++ src/main/java/com/example/tournaments_backend/TournamentsBackendApplication.java
@@ -19,7 +19,7 @@ public class TournamentsBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry
-					.addMapping("/api/v*/**")
+					.addMapping("/api/v1/auth/login")
 					.allowedOrigins("http://localhost:8081");
 			}
 		};
