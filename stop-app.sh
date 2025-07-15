0b2645e7de097f1dd2c719ebaec4a7d30c9509d5
Heri <hrodriguez1821@gmail.com>
2025-07-15 09:38:33 -0700

    Replaced two cmds with one cmd that stops database.

--- stop-app.sh
+++ stop-app.sh
@@ -3,8 +3,7 @@
 ID=$(sudo lsof -i:8080 | sed -n '2p' | awk '{print $2}')
 sudo kill -9 $ID
 # stop the database
-ID=$(sudo lsof -i:5432 | sed -n '2p' | awk '{print $2}')
-sudo kill -9 $ID
+sudo systemctl stop postgresql
 # stop the mail server
 ID=$(sudo lsof -i:1080 | sed -n '2p' | awk '{print $2}')
 sudo kill -9 $ID
