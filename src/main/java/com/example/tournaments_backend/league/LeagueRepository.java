34eac2051ef3ff0980a0061c2c023a5dc7a5648d
Heri <hrodriguez1821@gmail.com>
2025-07-16 10:26:19 -0700

    Added a method to get leagues after a certain start date.

--- src/main/java/com/example/tournaments_backend/league/LeagueRepository.java
+++ src/main/java/com/example/tournaments_backend/league/LeagueRepository.java
@@ -1,9 +1,12 @@
 package com.example.tournaments_backend.league;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeagueRepository extends JpaRepository<League, Long> {
-    
+    List<League> findByStartDateAfter(LocalDate date);
 }
