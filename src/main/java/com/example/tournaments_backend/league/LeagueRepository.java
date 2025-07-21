93f14af3f6ea3a6f383532610ea89f5eb826095c
Heri <hrodriguez1821@gmail.com>
2025-07-21 08:15:25 -0700

    Added a query to get leagues that are in progress and that have already ended.

--- src/main/java/com/example/tournaments_backend/league/LeagueRepository.java
+++ src/main/java/com/example/tournaments_backend/league/LeagueRepository.java
@@ -4,9 +4,20 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeagueRepository extends JpaRepository<League, Long> {
     List<League> findByStartDateAfter(LocalDate date);
+
+    @Query(value ="SELECT * FROM league l WHERE " +
+           "l.start_date <= :today AND " +
+           ":today <= l.start_date + (l.duration_in_weeks * INTERVAL '1 week')", nativeQuery = true)
+    List<League> findInProgressLeagues(@Param("today") LocalDate today);
+
+    @Query(value = "SELECT * FROM league l WHERE " +
+           ":today > l.start_date + (l.duration_in_weeks * INTERVAL '1 week')", nativeQuery = true)
+    List<League> findEndedLeagues(@Param("today") LocalDate today);
 }
