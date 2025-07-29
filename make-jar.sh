4199c6cd427f6c4db86cbe0277f52fdb9d4fd556
Heri <hrodriguez1821@gmail.com>
2025-07-28 18:28:20 -0700

    Start the datbase if it is not running. Make jar file.

--- make-jar.sh
+++ make-jar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # check if database is running
-if systemctl is-active --quiet postgresql; then
-    echo "PostgreSQL is running."
-else
-    # start the database if it is not running
-    echo "PostgreSQL is not running or failed to start."
+if ! systemctl is-active --quiet postgresql; then
+    # start database
+    sudo systemctl start postgresql
 fi
+# make jar file
+mvn clean package
