a9d4b5e3ac3783e19415cd4c135c47687e022c49
Heri <hrodriguez1821@gmail.com>
2025-09-23 14:40:19 -0700

    Added property to Team class to track the team owner.

--- src/main/java/com/example/tournaments_backend/team/Team.java
+++ src/main/java/com/example/tournaments_backend/team/Team.java
@@ -11,10 +11,13 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import lombok.EqualsAndHashCode;
@@ -40,6 +43,9 @@ public class Team {
     )
     @EqualsAndHashCode.Include
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Player owner;
     private String name;
     @JsonIgnore
     @ManyToMany(mappedBy = "teams")
