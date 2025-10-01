741c89ac40b9247d4d1dbd9190a4a98395b240fb
Heri <hrodriguez1821@gmail.com>
2025-10-01 08:25:20 -0700

    Fixed typo

--- src/main/java/com/example/tournaments_backend/player/PlayerRepository.java
+++ src/main/java/com/example/tournaments_backend/player/PlayerRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    List<Player> finAllByEmailIn(List<String> emails);
+    List<Player> findAllByEmailIn(List<String> emails);
     Optional<Player> findByEmail(String email);
 }
