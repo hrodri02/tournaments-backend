0ff70af18ef9e4c004aa2518d610798b0edae9b1
Heri <hrodriguez1821@gmail.com>
2025-10-14 13:03:02 -0700

    Added a method to get all the teams a player belongs to.

--- src/main/java/com/example/tournaments_backend/team/TeamRepository.java
+++ src/main/java/com/example/tournaments_backend/team/TeamRepository.java
@@ -1,9 +1,11 @@
 package com.example.tournaments_backend.team;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    
+    List<Team> findByPlayers_Id(Long playerId);
 }
