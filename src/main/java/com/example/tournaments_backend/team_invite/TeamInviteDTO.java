88347eeaf85e90a6c5dd76b3b45c6f12a74fa63c
Heri <hrodriguez1821@gmail.com>
2025-10-20 12:00:52 -0700

    Replaced playerId with player object to display it on the frontend.

--- src/main/java/com/example/tournaments_backend/team_invite/TeamInviteDTO.java
+++ src/main/java/com/example/tournaments_backend/team_invite/TeamInviteDTO.java
@@ -7,24 +7,27 @@ import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import com.example.tournaments_backend.player.PlayerDTO;
+
 @Getter
 @NoArgsConstructor
 public class TeamInviteDTO {
     private Long id;
     private TeamInviteStatus status;
     private Long teamId;
-    private Long playerId;
+    private PlayerDTO player;
     private LocalDateTime createdAt;
 
     public TeamInviteDTO(TeamInvite teamInvite) {
         this.id = teamInvite.getId();
         this.status = teamInvite.getStatus();
         this.teamId = teamInvite.getTeam().getId();
-        this.playerId = teamInvite.getInvitee().getId();
+        this.player = new PlayerDTO(teamInvite.getInvitee());
         this.createdAt = teamInvite.getCreatedAt();
     }
 
     public static List<TeamInviteDTO> convert(List<TeamInvite> invites) {
+        if (invites == null || invites.size() == 0) return List.of();
         return invites
             .stream()
             .map(TeamInviteDTO::new)
