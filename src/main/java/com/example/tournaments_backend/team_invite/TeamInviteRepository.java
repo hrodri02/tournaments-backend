adbf6a393ec890b2e586291066f63579dd364514
Heri <hrodriguez1821@gmail.com>
2025-11-14 11:04:12 -0800

    Added a method to search for a team invite by team id and player id.

--- src/main/java/com/example/tournaments_backend/team_invite/TeamInviteRepository.java
+++ src/main/java/com/example/tournaments_backend/team_invite/TeamInviteRepository.java
@@ -1,6 +1,7 @@
 package com.example.tournaments_backend.team_invite;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeamInviteRepository extends JpaRepository<TeamInvite, Long> {
      List<TeamInvite> findAllByInviteeId(Long inviteeId);
+     Optional<TeamInvite> findByTeamIdAndInviteeId(Long teamId, Long inviteeId);
 }
-
