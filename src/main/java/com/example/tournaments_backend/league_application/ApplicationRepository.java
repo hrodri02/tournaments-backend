2a801e21f6501ae4520f7869838da4869547026e
Heri <hrodriguez1821@gmail.com>
2025-11-19 10:15:23 -0800

    Added a method to get all the applications by teamId.

--- src/main/java/com/example/tournaments_backend/league_application/ApplicationRepository.java
+++ src/main/java/com/example/tournaments_backend/league_application/ApplicationRepository.java
@@ -1,9 +1,11 @@
 package com.example.tournaments_backend.league_application;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    
+    List<Application> findAllByTeamId(Long teamId);
 }
