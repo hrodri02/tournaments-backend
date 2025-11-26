c92a66ca153065d05ad61b1d9c8fb9774aacfcba
Heri <hrodriguez1821@gmail.com>
2025-11-26 08:52:25 -0800

    Added a property for the stats of a game.

--- src/main/java/com/example/tournaments_backend/game/GameDTO.java
+++ src/main/java/com/example/tournaments_backend/game/GameDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.tournaments_backend.game_stat.GameStatDTO;
 import com.example.tournaments_backend.team.TeamDTO;
 
 import lombok.Getter;
@@ -17,6 +18,7 @@ public class GameDTO {
     private Integer durationInMinutes;
     private TeamDTO homeTeam;
     private TeamDTO awayTeam;
+    private List<GameStatDTO> stats;
 
     public GameDTO(Game game) {
         this.id = game.getId();
@@ -26,6 +28,10 @@ public class GameDTO {
         this.durationInMinutes = game.getDurationInMinutes();
         this.homeTeam = new TeamDTO(game.getHomeTeam());
         this.awayTeam = new TeamDTO(game.getAwayTeam());
+        this.stats = game.getGameStats()
+                            .stream()
+                            .map(GameStatDTO::new)
+                            .toList();
     }
 
     public static List<GameDTO> convertGames(List<Game> games) {
