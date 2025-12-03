7b67ca930c0c91bbdce4b7f107c7facac2fe28ad
Heri <hrodriguez1821@gmail.com>
2025-12-03 11:56:31 -0800

    Added HttpStatus property to class and replaced message with errorKey that a client will use to translate error messages.

--- src/main/java/com/example/tournaments_backend/exception/ErrorDetails.java
+++ src/main/java/com/example/tournaments_backend/exception/ErrorDetails.java
@@ -2,12 +2,15 @@ package com.example.tournaments_backend.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ErrorDetails {
+    private HttpStatus status;
+    private String errorKey;
     private LocalDateTime timestamp;
-    private String message;
 }
