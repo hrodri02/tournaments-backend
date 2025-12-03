cb60f1bc2a1bfa90bab4e9bcbc8773db0be98e79
Heri <hrodriguez1821@gmail.com>
2025-12-03 12:00:02 -0800

    Modified the constructors of ErrorDetails to include the http status and the error key.

--- src/main/java/com/example/tournaments_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/tournaments_backend/exception/GlobalExceptionHandler.java
@@ -7,10 +7,16 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,25 +32,75 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<?> handleAuthenticationException(AuthenticationException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDetails(LocalDateTime.now(), ex.getMessage()));
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<?> handleExpiredTokenException(ExpiredJwtException ex) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorDetails(
+                    status, 
+                    ClientErrorKey.TOKEN_EXPIRED.name(), 
+                    LocalDateTime.now()
+                ));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleMissingTokenException(IllegalArgumentException ex) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorDetails(
+                    status, 
+                    ClientErrorKey.MISSING_TOKEN.name(), 
+                    LocalDateTime.now()
+                ));
+    }
+
+    @ExceptionHandler({SignatureException.class, MalformedJwtException.class, UnsupportedJwtException.class})
+    public ResponseEntity<?> handleAuthenticationException(JwtException ex) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorDetails(
+                    status, 
+                    ClientErrorKey.INVALID_TOKEN.name(), 
+                    LocalDateTime.now()
+                ));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> handleInvalidEnumValue(HttpMessageNotReadableException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         return ResponseEntity
-                        .status(HttpStatus.BAD_REQUEST)
-                        .body(new ErrorDetails(LocalDateTime.now(), ex.getMessage()));
+                .status(status)
+                .body(new ErrorDetails(
+                    status,
+                    ex.getMessage(),
+                    LocalDateTime.now()
+                ));
     }
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ErrorDetails> handleServiceException(ServiceException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDetails(LocalDateTime.now(), ex.getMessage()));
+        HttpStatus status = ex.getStatus();
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorDetails(
+                    status,
+                    ex.getErrorKey().name(),
+                    LocalDateTime.now()
+                ));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorDetails> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDetails(LocalDateTime.now(), ex.getMessage()));
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorDetails(
+                    status,
+                    ex.getMessage(),
+                    LocalDateTime.now()
+                ));
     }
 }
