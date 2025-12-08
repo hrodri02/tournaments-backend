8cfeb5b09f663f1d8083afe819397ef56cb7c1a4
Heri <hrodriguez1821@gmail.com>
2025-12-08 08:28:43 -0800

    Removed unused imports.

--- src/main/java/com/example/tournaments_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/tournaments_backend/exception/GlobalExceptionHandler.java
@@ -1,15 +1,12 @@
 package com.example.tournaments_backend.exception;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.security.authentication.BadCredentialsException;
