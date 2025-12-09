b8afbaed5d31525c96f0446fbeb80a2f5de9bd26
Heri <hrodriguez1821@gmail.com>
2025-12-09 10:41:31 -0800

    Converted the type of status to an int because jackson library was converting the HttpStatus property correctly to a number.

--- src/main/java/com/example/tournaments_backend/exception/ErrorDetails.java
+++ src/main/java/com/example/tournaments_backend/exception/ErrorDetails.java
@@ -5,30 +5,27 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ErrorDetails {
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
-    private HttpStatus status;
+    private int status;
     private String errorKey;
     private LocalDateTime timestamp;
     private List<ValidationErrorDetail> validationErrors;
 
     public ErrorDetails(HttpStatus status, String errorKey, LocalDateTime timestamp) {
         this.timestamp = timestamp;
-        this.status = status;
+        this.status = status.value();
         this.errorKey = errorKey;
         validationErrors = List.of();
     }
 
     public ErrorDetails(HttpStatus status, String errorKey, List<ValidationErrorDetail> validationErrors) {
         this.timestamp = LocalDateTime.now();
-        this.status = status;
+        this.status = status.value();
         this.errorKey = errorKey; // Use a general key like "VALIDATION_FAILED"
         this.validationErrors = validationErrors;
     }
