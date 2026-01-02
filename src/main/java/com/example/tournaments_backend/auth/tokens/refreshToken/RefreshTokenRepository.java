a8f6477c297d9f8b9b1d5b5e7f6d396a34735c06
Heri <hrodriguez1821@gmail.com>
2026-01-02 04:29:37 -0800

    Added a method to find a RefreshToken by the encoded token.

--- src/main/java/com/example/tournaments_backend/auth/tokens/refreshToken/RefreshTokenRepository.java
+++ src/main/java/com/example/tournaments_backend/auth/tokens/refreshToken/RefreshTokenRepository.java
@@ -1,9 +1,11 @@
 package com.example.tournaments_backend.auth.tokens.refreshToken;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    
+    Optional<RefreshToken> findByToken(String token);
 }
