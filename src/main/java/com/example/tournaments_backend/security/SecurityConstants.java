e8210e083ce3562e3358e70d661264bf91aac58f
Heri <hrodriguez1821@gmail.com>
2026-01-02 04:57:11 -0800

    Switched the expiration time for the refresh token and access token because I had them reversed. Using expiration time of 1 minute for access token for testing.

--- src/main/java/com/example/tournaments_backend/security/SecurityConstants.java
+++ src/main/java/com/example/tournaments_backend/security/SecurityConstants.java
@@ -1,9 +1,10 @@
 package com.example.tournaments_backend.security;
 
 public class SecurityConstants {
+    
     public static final String SECRET = "3cfa76ef14937c1c0ea519f8fc057a80fcd04a7420f8e8bcd0a7567c272e007b";
-    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 1_296_000_000; // 15 days
-    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 54_000_000; // 15 minutes
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 3_600_000; //  1 minute 54_000_000; // 15 minutes
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 1_296_000_000; // 15 days
     public static final String TOKEN_TYPE = "Bearer";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String AUTHORIZATION_HEADER = "Authorization";
