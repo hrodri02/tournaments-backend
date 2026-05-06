e858d9071b2f439400363498303a1ada18b6b3d7
Heri <hrodriguez1821@gmail.com>
2026-05-05 18:21:19 -0700

    Added ActiveProfiles annotation to main test class.

--- src/test/java/com/example/tournaments_backend/TournamentsBackendApplicationTests.java
+++ src/test/java/com/example/tournaments_backend/TournamentsBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.tournaments_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TournamentsBackendApplicationTests {
 
 	@Test
